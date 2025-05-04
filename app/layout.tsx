28a148d852b214eb55fe7b35e703061a0b35f1bf
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-05-05 04:10:18 +0600

    sidebar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "../components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,8 +18,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en-US">
-        <Header />
-        <body>{children}</body>
+        <body>
+          <Header />
+
+          <div className="flex min-h-screen ">
+            <Sidebar />
+            {/* sidebar  */}
+            <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide">
+              {children}
+            </div>
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
